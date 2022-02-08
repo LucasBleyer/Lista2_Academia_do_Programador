cd759969cd4e77200df8fdc69fade39df344f85f
Lucas Bleyer <lucasbleyer19@gmail.com>
2022-02-08 09:52:20 -0300

    add do while in somaAeB

--- SomaAeB.ConsoleApp/Program.cs
+++ SomaAeB.ConsoleApp/Program.cs
@@ -6,29 +6,40 @@ namespace SomaAeB.ConsoleApp
     {
         static void Main(string[] args)
         {
-            Console.Write("Digite o valor de A: ");
-            double a = Convert.ToDouble(Console.ReadLine());
-
-            Console.Write("Digite o valor de B: ");
-            double b = Convert.ToDouble(Console.ReadLine());
-
-            Console.Write("Digite o valor de C: ");
-            double c = Convert.ToDouble(Console.ReadLine());
-
-            double soma = a + b;
-
-            if (soma < c)
+            string opcao;
+            do
             {
-                Console.WriteLine("A soma de A + B é "+soma+" que é maior do que o valor de C");
-            }
-            else if (soma == c)
-            {
-                Console.WriteLine("A soma de A + B é "+soma+" que é igual ao valor de C");
-            }
-            else
-            {
-                Console.WriteLine("A soma de A + B é "+soma+" que é menor que o valor de C");
-            }
+                Console.WriteLine("--------------------------------------");
+                Console.Write("Digite o valor de A: ");
+                double a = Convert.ToDouble(Console.ReadLine());
+
+                Console.Write("Digite o valor de B: ");
+                double b = Convert.ToDouble(Console.ReadLine());
+
+                Console.Write("Digite o valor de C: ");
+                double c = Convert.ToDouble(Console.ReadLine());
+
+                double soma = a + b;
+
+                if (soma > c)
+                {
+                    Console.WriteLine("A soma de A + B é "+soma+" que é maior do que o valor de C");
+                }
+                else if (soma == c)
+                {
+                    Console.WriteLine("A soma de A + B é "+soma+" que é igual ao valor de C");
+                }
+                else
+                {
+                    Console.WriteLine("A soma de A + B é "+soma+" que é menor que o valor de C");
+                }
+
+                Console.Write(" [Enter] para continuar ou [s] para Sair: ");
+                opcao = Console.ReadLine();
+
+            } while (opcao != "s");
+
+            
             Console.ReadLine();
         }
     }
