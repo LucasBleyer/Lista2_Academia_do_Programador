fc9f5a038ac2f56eef98509c3425bf69efe7560e
Lucas Bleyer <lucasbleyer19@gmail.com>
2022-02-07 23:31:41 -0300

    add soma a e b

--- SomaAeB.ConsoleApp/Program.cs
+++ SomaAeB.ConsoleApp/Program.cs
@@ -6,7 +6,30 @@ namespace SomaAeB.ConsoleApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.Write("Digite o valor de A: ");
+            double a = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Digite o valor de B: ");
+            double b = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Digite o valor de C: ");
+            double c = Convert.ToDouble(Console.ReadLine());
+
+            double soma = a + b;
+
+            if (soma < c)
+            {
+                Console.WriteLine("A soma de A + B é "+soma+" que é maior do que o valor de C");
+            }
+            else if (soma == c)
+            {
+                Console.WriteLine("A soma de A + B é "+soma+" que é igual ao valor de C");
+            }
+            else
+            {
+                Console.WriteLine("A soma de A + B é "+soma+" que é menor que o valor de C");
+            }
+            Console.ReadLine();
         }
     }
 }
